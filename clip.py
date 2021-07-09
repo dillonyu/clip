0709465e1ade29a4b4d2f7e48e1fbebdf1c8721e
Dillon Yu <86173315+dillonyu@users.noreply.github.com>
2021-07-09 15:00:16 -0700

    added feature to print clipboard contents

--- clip.py
+++ clip.py
@@ -8,11 +8,13 @@ entries = []
 # Updates the clipboard with new copied text
 def update_board():
     entry = pyperclip.paste()
-    if type(entry) != None and entry not in entries:
+    if entry != None and entry not in entries:
         entries.append(entry)
         # board is the file object storing the clipboard
-        board = open("clip.txt", "a") # by default, the entries are stored in a file called "clip.txt"
+        board = open("clip.txt", "a+") # by default, the entries are stored in a file called "clip.txt"
         board.write('\n' + entry)
+        board.seek(0)
+        print(board.read())
         board.close()
 
         # print(entries)
