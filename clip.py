103a1709d445a464e3d9f35d5d43100f0de7890c
Dillon Yu <86173315+dillonyu@users.noreply.github.com>
2021-07-09 14:46:15 -0700

    fixed formatting bugs

--- clip.py
+++ clip.py
@@ -8,11 +8,11 @@ entries = []
 # Updates the clipboard with new copied text
 def update_board():
     entry = pyperclip.paste()
-    if entry not in entries:
+    if type(entry) != None and entry not in entries:
         entries.append(entry)
         # board is the file object storing the clipboard
         board = open("clip.txt", "a") # by default, the entries are stored in a file called "clip.txt"
-        board.write(entry)
+        board.write('\n' + entry)
         board.close()
 
         # print(entries)
