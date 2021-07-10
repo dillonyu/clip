597123982a0c145eee0b8273cfa08501afec3c19
Dillon Yu <86173315+dillonyu@users.noreply.github.com>
2021-07-10 15:52:16 -0700

    implemented clipboard updating in background

--- clip.py
+++ clip.py
@@ -1,7 +1,6 @@
-from multiprocessing import Process
-
 import pyperclip
 import os
+import tkinter as tk
 
 # entries stores the clipboard contents
 entries = []
@@ -10,50 +9,29 @@ entries = []
 # Updates the clipboard with new copied text
 def update_board():
     entry = pyperclip.paste()
-    if entry != None and entry not in entries:
+    if entry is not None and entry not in entries:
         entries.append(entry)
         # board is the file object storing the clipboard
-        board = open("clip.txt", "a+") # by default, the entries are stored in a file called "clip.txt"
+        board = open("clip.txt", "a+")  # by default, the entries are stored in a file called "clip.txt"
         board.write('\n' + entry)
-        # board.seek(0)
-        # print(board.read())
         board.close()
+    root.after(10, update_board)
 
-        # print(entries)
 
-# def runInParallel(*fns):
-#   proc = []
-#   for fn in fns:
-#     p = Process(target=fn)
-#     p.start()
-#     proc.append(p)
-#   for p in proc:
-#     p.join()
+def test():
+    while True:
+        print('View Clipboard (view)')
+        response = input()
+        if response == 'view':
+            break
+    print('Viewing Clipboard...')
 
-# def test():
-#         while True:
-#             print('View Clipboard (view)')
-#             response = str(input())
-#             if response == 'view':
-#                 break
-#         print('Viewing Clipboard...')
 
 # Main Program
-# if __name__ == '__main__':
-#     p1 = Process(target=update_board)
-#     p1.start()
-#     p2 = Process(target=test)
-#     p2.start()
-#     p1.join()
-#     p2.join()
-#     os.chdir('/Users/dillonyu/Desktop/') # will change to universal Desktop location later
-#     print('Welcome to Clip! What would you like to do?')
-#     # while True:
-#         # update_board()
-#     while True:
-#         runInParallel(update_board, test)
-
-os.chdir('/Users/dillonyu/Desktop/') # will change to universal Desktop location later
-print('Welcome to Clip! What would you like to do?')
-while True:
+if __name__ == '__main__':
+    root = tk.Tk()
+    os.chdir('/Users/dillonyu/Desktop/')  # will change to universal Desktop location later
+    w = tk.Label(root, text="Hello Tkinter!")
+    w.pack()
     update_board()
+    root.mainloop()
