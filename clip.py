9a2f6c19570f31978076d1ff678fe4b34a625512
Dillon Yu <86173315+dillonyu@users.noreply.github.com>
2021-07-11 12:21:35 -0700

    added set number of buttons

--- clip.py
+++ clip.py
@@ -2,27 +2,47 @@ import pyperclip
 import os
 import tkinter as tk
 
-# entries stores the clipboard contents
+# entries stores the clipboard contents as strings
 entries = []
+# buttons stores the clipboard contents as button objects
+buttons = []
+# nextButton stores the index of the next button to store the latest entry
+nextButton = 0
 root = tk.Tk()
+frame = tk.Frame(root)
+frame.pack()
 
 
 # Updates the clipboard with new copied text
 def update_board():
+    global nextButton
     entry = pyperclip.paste()
-    if entry is not None and entry not in entries:
+    if entry is not None and not entry.isspace() and entry not in entries:
         entries.append(entry)
-        label = tk.Label(root, text=entry)
-        label.pack()
-        # board is the file object storing the clipboard
-        board = open("clip.txt", "a+")  # by default, the entries are stored in a file called "clip.txt"
-        board.write('\n' + entry)
-        board.close()
+        if nextButton > len(buttons):
+            nextButton = 0
+        buttons[nextButton].config(text=entry)
+        nextButton += 1
     root.after(10, update_board)
+
+
+def copy_text():
+    print('Text copied!')
 
 
 # Main Program
 if __name__ == '__main__':
-    os.chdir('/Users/dillonyu/Desktop/')  # will change to universal Desktop location later
+    # os.chdir('/Users/dillonyu/Desktop/')  # will change to universal Desktop location later
+    for i in range(10):
+        button = tk.Button(frame,
+                           text="",
+                           anchor='w',
+                           justify='left',
+                           height=3,
+                           width=100,
+                           wraplengt=850,
+                           command=copy_text)
+        button.pack()
+        buttons.append(button)
     update_board()
     root.mainloop()
