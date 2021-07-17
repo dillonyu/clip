c6416353bd38ff881940a04cb4af51e389314743
Dillon Yu <86173315+dillonyu@users.noreply.github.com>
2021-07-17 16:46:01 -0700

    various code refinements

--- clip.py
+++ clip.py
@@ -10,90 +10,81 @@ entry_buttons = []
 del_buttons = []
 # expand_buttons stores the expand buttons
 expand_buttons = []
-# next_button stores the index of the next button to store the latest entry
-next_button = 0
+# next_space stores the index of the next available space to store the newest entry
+next_space = 0
+# full is True when all 10 spots in the clipboard are filled, and False otherwise
+full = False
+
 root = tk.Tk()
 root.geometry("616x557")
-frame = tk.Frame(root)
-full = False
+root.title('Clip')
 
 
 # Updates the clipboard with any new copied text
 def update_board():
-    global next_button
     entry = pyperclip.paste()
-    if entry is not None and not entry.isspace() and entry != '' and entry not in entries:
+    if entry != '' and entry not in entries:
         entries.append(entry)
         add_entry(entry)
     root.after(10, update_board)
 
 
-# Copies the input text
-def copy_text(text):
-    if text.isspace():
-        return
-    pyperclip.copy(text)
-
-
-# Shifts all clipboard contents up when not enough space is available for a new entry
-def shift_entries_up(i):
-    # if deleting the last entry
-    if i == len(entry_buttons):
-        entry_buttons[i - 1].config(text='')
+# Shifts all clipboard content up from index k, deleting the entry at index k - 1
+def shift_entries_up(k):
     if full:
         del entries[0]
-    for a in range(i, len(entry_buttons)):
+    for a in range(k, len(entry_buttons)):
         next_text = entry_buttons[a].cget('text')
         entry_buttons[a - 1].config(text=next_text)
-        if a == len(entry_buttons) - 1:
-            entry_buttons[a].config(text='')
+    entry_buttons[len(entry_buttons) - 1].config(text='')
 
 
 # Opens a new window to display the full text on a clipboard entry
 def expand_entry(entry):
-    ans = mb.askokcancel('Full Text', 'Full Text:\n\n' + entry + '\n\nCopy Text?')
+    ans = mb.askokcancel('Full Text', 'Full Text:\n\n \" ' + entry + " \"" + '\n\nCopy Text?')
     if ans:
         pyperclip.copy(entry)
 
 
 # Adds the input text as an entry to the clipboard
 def add_entry(new_text):
-    global next_button
+    global next_space
     global full
-    if next_button >= len(entry_buttons):
+    # adjusts accordingly if the clipboard is full
+    if next_space >= len(entry_buttons):
         full = True
         shift_entries_up(1)
-        next_button -= 1
-    entry_buttons[next_button].config(text=new_text, state='normal')
-    del_buttons[next_button].config(state='normal')
-    expand_buttons[next_button].config(state='normal')
-    next_button += 1
+        next_space -= 1
+    entry_buttons[next_space].config(text=new_text, state='normal')
+    del_buttons[next_space].config(state='normal')
+    expand_buttons[next_space].config(state='normal')
+    next_space += 1
 
 
 # Deletes an entry based on the input index
-def del_entry(i):
-    global next_button
+def del_entry(d):
+    global next_space
     global full
-    del entries[i]
+    del entries[d]
     pyperclip.copy('')
     full = False
-    shift_entries_up(i + 1)
-    next_button -= 1
-    del_buttons[next_button].config(state='disabled')
-    expand_buttons[next_button].config(state='disabled')
-    entry_buttons[next_button].config(state='disabled')
+    shift_entries_up(d + 1)
+    next_space -= 1
+    del_buttons[next_space].config(state='disabled')
+    expand_buttons[next_space].config(state='disabled')
+    entry_buttons[next_space].config(state='disabled')
 
 
 # Opens the clear all warning window
 def open_clr_warning():
-    global next_button
+    global next_space
     ans = mb.askquestion('Clear Clipboard', 'Are you sure you want to clear the clipboard?')
     if ans == 'yes':
-        for i in range(len(entry_buttons)):
-            entry_buttons[i].config(text='', state='disabled')
-            expand_buttons[i].config(state='disabled')
-            del_buttons[i].config(state='disabled')
-        next_button = 0
+        for b in range(len(entry_buttons)):
+            entry_buttons[b].config(text='', state='disabled')
+            expand_buttons[b].config(state='disabled')
+            del_buttons[b].config(state='disabled')
+        next_space = 0
         entries.clear()
         pyperclip.copy('')
 
@@ -107,7 +98,7 @@ for i in range(10):
                        width=57,
                        wraplength=500,
                        state='disabled')
-    button.config(command=lambda b=button: copy_text(b['text']))
+    button.config(command=lambda b=button: pyperclip.copy(b['text']))
     button.grid(row=i, column=0)
     entry_buttons.append(button)
 
@@ -134,7 +125,7 @@ for i in range(10):
 clr_all_button = tk.Button(text="Clear All",
                            justify='left',
                            height=2,
-                           width=57, )
+                           width=57)
 clr_all_button.grid(row=10, column=0)
 clr_all_button.config(command=open_clr_warning)
 
